e16c75047183ed84c69e3cc1b7fdf2169de79222
pomodorozhong <pomodorozhong@gmail.com>
2023-01-22 09:38:05 +0800

    feat✨(App): use `eventEngine.js` and `PlayerStatus`

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import TextBubbles from "./textBubbles.js";
 import React, { useState } from "react";
+import { pickNextEvent, handleEvent } from "./eventEngine.js";
+import PlayerStatus from "./playerStatus.js";
 
 const messages = [
   {
@@ -25,17 +27,36 @@ const messages = [
 
 function App() {
   const [displayMessages, setDisplayMessages] = useState([]);
+  const [tempString, setTempString] = useState("");
+  const [playerAttributes, setPlayerAttributes] = useState({
+    health: 100,
+    experience: 0,
+    gold: 0,
+    strength: 51,
+    intelligence: 51,
+    courage: 51,
+  });
+  const [tempAttributes, setTempAttributes] = useState({});
 
   const handleClick = () => {
-    const index =
-      displayMessages.length >= messages.length
-        ? messages.length - 1
-        : displayMessages.length;
-    const newMessage = messages[index];
-    setDisplayMessages([...displayMessages, newMessage]);
+    if (tempString !== "") {
+      setDisplayMessages([...displayMessages, { text: tempString }]);
+      setTempString("");
+      setPlayerAttributes({ ...playerAttributes, ...tempAttributes });
+    } else {
+      const id = pickNextEvent(playerAttributes);
+      const result = handleEvent(id, playerAttributes);
+      console.log(result);
+
+      setDisplayMessages([...displayMessages, { text: result[0] }]);
+      setTempString(result[1]);
+
+      setTempAttributes({ ...playerAttributes, ...result[2] });
+    }
   };
   return (
     <div className="App">
+      <PlayerStatus player={playerAttributes} />
       <div onClick={handleClick}>
         <TextBubbles messages={displayMessages} />
       </div>
