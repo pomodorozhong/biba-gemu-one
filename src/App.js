8a26033bbd7f7956af275aa240e4960a782e1ade
pomodorozhong <pomodorozhong@gmail.com>
2023-02-22 23:31:09 +0800

    chore⚙️(`App`): remove unused `messages`

--- src/App.js
+++ src/App.js
@@ -5,26 +5,6 @@ import React, { useState } from "react";
 import { pickNextEvent, handleEvent } from "./eventEngine.js";
 import PlayerStatus from "./playerStatus.js";
 
-const messages = [
-  {
-    text: "The ship approaches a rocky coastline, danger looms as the fate of the ship and crew hang in the balance.",
-  },
-  {
-    text: "The ship sailed through the water, its sails catching the wind. The captain, who was experienced and well-trained, carefully guided the vessel through the rough seas.",
-  },
-  { text: "The ship's crew worked together to keep it in good condition." },
-  {
-    text: "As they came close to a rocky coastline, the captain made a wise decision to steer the ship away from danger, and they sailed safely past the hazardous waters.",
-  },
-  { text: "5" },
-  { text: "6" },
-  { text: "7" },
-  { text: "8" },
-  { text: "9" },
-  { text: "10" },
-  { text: "11" },
-];
-
 function App() {
   const [displayMessages, setDisplayMessages] = useState([]);
   const [tempString, setTempString] = useState("");
